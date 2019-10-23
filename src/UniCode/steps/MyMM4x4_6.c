9946d004823c5f2a5bea6869874553497785d955
CaptainDuke <captainduke328@gmail.com>
2019-10-23 14:58:33 +0800

    4x4 update

--- src/UniCode/steps/MyMM4x4_6.c
+++ src/UniCode/steps/MyMM4x4_6.c
@@ -36,11 +36,8 @@ void AddDot4x4(int k, float *a, int lda,  float *b, int ldb, float *c, int ldc )
     c_20_reg, c_21_reg, c_22_reg, c_23_reg, 
     c_30_reg, c_31_reg, c_32_reg, c_33_reg,
 
-    // store A(i, p)
-    a_0p_reg,
-    a_1p_reg,
-    a_2p_reg,
-    a_3p_reg;
+    // store B(p, 0-3)
+    b_p0_reg, b_p1_reg, b_p2_reg, b_p3_reg;
 
     c_00_reg = 0.0; c_01_reg = 0.0; c_02_reg = 0.0; c_03_reg = 0.0; 
     c_10_reg = 0.0; c_11_reg = 0.0; c_12_reg = 0.0; c_13_reg = 0.0; 
@@ -48,37 +45,37 @@ void AddDot4x4(int k, float *a, int lda,  float *b, int ldb, float *c, int ldc )
     c_30_reg = 0.0; c_31_reg = 0.0; c_32_reg = 0.0; c_33_reg = 0.0;
 
     for(int p = 0; p < k; p++){
-      a_0p_reg = A(0,p);
-      a_1p_reg = A(1,p);
-      a_2p_reg = A(2,p);
-      a_3p_reg = A(3,p);
+      b_p0_reg = B(p,0);
+      b_p1_reg = B(p,1);
+      b_p2_reg = B(p,2);
+      b_p3_reg = B(p,3);
 
-      c_00_reg += a_0p_reg * B(p, 0);
-      c_01_reg += a_0p_reg * B(p, 1);
-      c_02_reg += a_0p_reg * B(p, 2);
-      c_03_reg += a_0p_reg * B(p, 3);
+      c_00_reg += A(0, p) * b_p0_reg;
+      c_01_reg += A(0, p) * b_p1_reg;
+      c_02_reg += A(0, p) * b_p2_reg;
+      c_03_reg += A(0, p) * b_p3_reg;
 
-      c_10_reg += a_1p_reg * B(p, 0);
-      c_11_reg += a_1p_reg * B(p, 1);
-      c_12_reg += a_1p_reg * B(p, 2);
-      c_13_reg += a_1p_reg * B(p, 3);
+      c_10_reg += A(1, p) * b_p0_reg;
+      c_11_reg += A(1, p) * b_p1_reg;
+      c_12_reg += A(1, p) * b_p2_reg;
+      c_13_reg += A(1, p) * b_p3_reg;
 
-      c_20_reg += a_2p_reg * B(p, 0);
-      c_21_reg += a_2p_reg * B(p, 1);
-      c_22_reg += a_2p_reg * B(p, 2);
-      c_23_reg += a_2p_reg * B(p, 3);
+      c_20_reg += A(2, p) * b_p0_reg;
+      c_21_reg += A(2, p) * b_p1_reg;
+      c_22_reg += A(2, p) * b_p2_reg;
+      c_23_reg += A(2, p) * b_p3_reg;
 
-      c_30_reg += a_3p_reg * B(p, 0);
-      c_31_reg += a_3p_reg * B(p, 1);
-      c_32_reg += a_3p_reg * B(p, 2);
-      c_33_reg += a_3p_reg * B(p, 3);
+      c_30_reg += A(3, p) * b_p0_reg;
+      c_31_reg += A(3, p) * b_p1_reg;
+      c_32_reg += A(3, p) * b_p2_reg;
+      c_33_reg += A(3, p) * b_p3_reg;
 
     }
 
     C(0, 0) = c_00_reg; C(0, 1) = c_01_reg; C(0, 2) = c_02_reg; C(0, 3) = c_03_reg;
-    C(1, 0) = c_10_reg; C(0, 1) = c_11_reg; C(1, 2) = c_12_reg; C(1, 3) = c_13_reg;
-    C(2, 0) = c_20_reg; C(0, 1) = c_21_reg; C(2, 2) = c_22_reg; C(2, 3) = c_23_reg;
-    C(3, 0) = c_30_reg; C(0, 1) = c_31_reg; C(3, 2) = c_32_reg; C(3, 3) = c_33_reg;
+    C(1, 0) = c_10_reg; C(1, 1) = c_11_reg; C(1, 2) = c_12_reg; C(1, 3) = c_13_reg;
+    C(2, 0) = c_20_reg; C(2, 1) = c_21_reg; C(2, 2) = c_22_reg; C(2, 3) = c_23_reg;
+    C(3, 0) = c_30_reg; C(3, 1) = c_31_reg; C(3, 2) = c_32_reg; C(3, 3) = c_33_reg;
   
 }
 
